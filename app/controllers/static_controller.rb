39e45ce739e0e8aa84d160a842a6a518f6793190
Arne Eilermann <arne@kleinerdrei.net>
2018-10-30 13:33:22 +0100

    Added a controller for static pages.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def privacy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'home', to: 'static#home'
+  get 'contact', to: 'static#contact'
+  get 'privacy', to: 'static#privacy'
+
   resources :people, param: :random_id
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'static#home'
 end
