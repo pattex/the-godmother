4105dbe07cca693b46dfd56a504a62041cec2fbf
Arne Eilermann <arne@kleinerdrei.net>
2018-10-31 17:14:48 +0100

    Added nicer flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_messages(f)
+    translate_name = { notice: 'success', alert: 'danger' }
+
+    f.map { |name, msg|
+      content_tag(:div, h(msg), class: "alert alert-#{translate_name[name.to_sym]}")
+    }.join("\n")
+  end
 end
