968bfe856860a6caf1ef904d567f1734324a74ec
Arne Eilermann <arne@kleinerdrei.net>
2018-12-09 17:26:25 +0100

    Added filters to people lists.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -2,4 +2,16 @@ module PeopleHelper
   def tags
     Tag.where(active: true).map { |t| html_escape(t.name) }
   end
+
+  def state_batches
+    {
+      unverified: 'secondary',
+      verified: 'primary',
+      okay: 'primary',
+      waiting: 'warning',
+      in_group: 'primary',
+      declined: 'danger',
+      done: 'sucess'
+    }
+  end
 end
