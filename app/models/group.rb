dec42ee0412c2426719d14311ad3ff77eee0b333
Arne Eilermann <arne@kleinerdrei.net>
2018-12-20 15:39:08 +0100

    Added a group list.

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,20 @@ class Group < ApplicationRecord
   has_many :mentees, class_name: 'Person'
 
   validates :mentors, presence: true, on: :create
+
+  def mentors
+    Person.where(group_id: self.id).where(role: 2)
+  end
+
+  def mentor_ids
+    self.mentors.map { |m| m.id }
+  end
+
+  def mentees
+    Person.where(group_id: self.id).where(role: 1)
+  end
+
+  def mentee_ids
+    self.mentees.map { |m| m.id }
+  end
 end
